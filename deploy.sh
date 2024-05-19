f6748c2c25fe1aa90a871e5e84a2028245ec6964
manjeetsingh53 <manjeetkumar53@gmail.com>
2024-05-19 15:09:07 +0200

    update venv ref

--- deploy.sh
+++ deploy.sh
@@ -11,15 +11,23 @@ sudo mv  * /var/www/ai-text-generator
 
 # Navigate to the app directory
 cd /var/www/ai-text-generator/
+
+# Create and activate virtual environment
+echo "creating virtual environment"
+python3 -m venv venv
+source venv/bin/activate
+
+# Move environment file
 sudo mv env .env
 
-sudo apt-get update
+# Install Python and pip
 echo "installing python and pip"
+sudo apt-get update
 sudo apt-get install -y python3 python3-pip
 
 # Install application dependencies from requirements.txt
 echo "Install application dependencies from requirements.txt"
-sudo pip install -r requirements.txt
+pip install -r requirements.txt
 
 # Update and install Nginx if not already installed
 if ! command -v nginx > /dev/null; then
@@ -53,9 +61,9 @@ fi
 sudo pkill gunicorn
 sudo rm -rf myapp.sock
 
-# # Start Gunicorn with the Flask application
-# # Replace 'server:app' with 'yourfile:app' if your Flask instance is named differently.
-# # gunicorn --workers 3 --bind 0.0.0.0:8000 server:app &
+# Start Gunicorn with the Flask application
+# Replace 'server:app' with 'yourfile:app' if your Flask instance is named differently.
+# gunicorn --workers 3 --bind 0.0.0.0:8000 server:app &
 echo "starting gunicorn"
 sudo gunicorn --workers 3 --bind unix:myapp.sock  appserver:app --user www-data --group www-data --daemon
 echo "started gunicorn 🚀"
